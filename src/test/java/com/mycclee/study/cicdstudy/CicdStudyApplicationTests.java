01526f84434c4d9893ab5ad2c5e56b2646a20867
authorLi <www.524894052@qq.com>
2022-03-12 00:17:54 +0800

    Initial commit

---
+++ src/test/java/com/mycclee/study/cicdstudy/CicdStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycclee.study.cicdstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
